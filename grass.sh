169c68af315e9ae4022bb16f43664d6a1741cf63
Dan Clegg <dgclegg@gmail.com>
2018-02-14 21:22:10 -0700

    Adding second boot script

--- grass.sh
+++ grass.sh
@@ -10,3 +10,7 @@ sudo addgroup --system ggc_group
 # Install sqlite3
 sudo apt-get update
 sudo apt-get install -y sqlite3
+
+# Update kernel
+sudo apt-get install -y rpi-update
+sudo rpi-update b81a11258fc911170b40a0b09bbd63c84bc5ad59
