a71085472936b0ec52a66db1282d82c97a6d6692
Dan Clegg <dgclegg@gmail.com>
2018-02-14 21:20:06 -0700

    Adding auto yes

--- grass.sh
+++ grass.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 # This script is called automatically by `pi-setup.sh` to run a batch of Pi setup commands that require sudo permissions
+# Source: https://docs.aws.amazon.com/greengrass/latest/developerguide/module1.html
+
+# Add greengrass user & group
+sudo adduser --system ggc_user
+sudo addgroup --system ggc_group
+
+# Install sqlite3
+sudo apt-get update
+sudo apt-get install -y sqlite3
